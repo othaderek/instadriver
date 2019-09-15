8a9468e2b9f23b1f0c449f0d16e05d84f1d222d2
othaderek <otha.hernandez@gmail.com>
2019-09-15 00:02:12 -0400

    Got the login feature working.

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 
+const {Builder, By, Key, until} = require('selenium-webdriver');
 
-var webdriver = require('selenium-webdriver'),
-  by = webdriver.By,
-  until = webdriver.until;
+let username = "username";
+let password = "password";
+let driver = new Builder().forBrowser('chrome').build();
 
-var driver = new webdriver.Builder()
-  .forBrowser('chrome')
-  .build();
+const startBrower = async () => {
+  await driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher');
+  await driver.sleep(1000);
+  // let inputForUsername = await driver.findElement(By.name('username'))
+  // let inputForPassword = await driver.findElement(By.name('password'))
+  // let result = await loginToAccount(inputForUsername, inputForPassword);
+  console.log();
 
-  driver.get('https://www.instagram.com/');
+};
+startBrower();
+
+const loginToAccount = (username, password) => {
+  username.sendKeys(username);
+  password.sendKeys(password, Key.RETURN)
+}
