9baa062d25055c1e204f414c1ac4a461db8af653
othaderek <otha.hernandez@gmail.com>
2019-09-15 00:08:36 -0400

    Added comments and uncommented essential lines.

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+/*
 
+REMEMBER TO DELETE YOUR LOGIN CREDENTIALS BEFORE GIT PUSHING!!!!!!
+REMEMBER TO DELETE YOUR LOGIN CREDENTIALS BEFORE GIT PUSHING!!!!!!
+REMEMBER TO DELETE YOUR LOGIN CREDENTIALS BEFORE GIT PUSHING!!!!!!
+REMEMBER TO DELETE YOUR LOGIN CREDENTIALS BEFORE GIT PUSHING!!!!!!
+REMEMBER TO DELETE YOUR LOGIN CREDENTIALS BEFORE GIT PUSHING!!!!!!
+REMEMBER TO DELETE YOUR LOGIN CREDENTIALS BEFORE GIT PUSHING!!!!!!
+REMEMBER TO DELETE YOUR LOGIN CREDENTIALS BEFORE GIT PUSHING!!!!!!
+REMEMBER TO DELETE YOUR LOGIN CREDENTIALS BEFORE GIT PUSHING!!!!!!
+REMEMBER TO DELETE YOUR LOGIN CREDENTIALS BEFORE GIT PUSHING!!!!!!
+
+*/
 const {Builder, By, Key, until} = require('selenium-webdriver');
 
 let username = "username";
@@ -8,9 +20,9 @@ let driver = new Builder().forBrowser('chrome').build();
 const startBrower = async () => {
   await driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher');
   await driver.sleep(1000);
-  // let inputForUsername = await driver.findElement(By.name('username'))
-  // let inputForPassword = await driver.findElement(By.name('password'))
-  // let result = await loginToAccount(inputForUsername, inputForPassword);
+  let inputForUsername = await driver.findElement(By.name('username'))
+  let inputForPassword = await driver.findElement(By.name('password'))
+  let result = await loginToAccount(inputForUsername, inputForPassword);
   console.log();
 
 };
